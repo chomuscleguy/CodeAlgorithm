cb3c638bda30417a7181760b35b9be3c1d59f6d3
조성우 <chomuscleguy@gmail.com>
2023-11-17 09:38:59 +0900

    commit

--- CodeAlgorithm/003quotient.cs
+++ CodeAlgorithm/003quotient.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace CodeAlgorithm
 {
-    //문제 설명
-    //정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
-    //제한사항
-    //0 < num1 ≤ 100
-    //0 < num2 ≤ 100
 
     public class Quotient
     {
+        //문제 설명
+        //정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+        //제한사항
+        //0 < num1 ≤ 100
+        //0 < num2 ≤ 100
         public int solution(int num1, int num2)
         {
             int answer = 0;
