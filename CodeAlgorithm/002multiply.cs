1009c8de8f4ee269642497ff4baf6907c24fe291
조성우 <chomuscleguy@gmail.com>
2023-11-17 09:36:23 +0900

    commit

--- CodeAlgorithm/002multiply.cs
+++ CodeAlgorithm/002multiply.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace CodeAlgorithm
 {
-    internal class Solution
+
+    public class Multiply
     {
+        //문제 설명
+        //정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+        //제한사항
+        //0 ≤ num1 ≤ 100
+        //0 ≤ num2 ≤ 100
+        public int solution(int num1, int num2)
+        {
+            int answer = 0;
+            if (num1 >= 0 && num1 <= 100 && num2 >= 0 && num2 <= 100)
+            {     
+                answer = num1 * num2;
+            }
+            return answer;
+        }
     }
 }
--- CodeAlgorithm/003quotient.cs
+++ CodeAlgorithm/003quotient.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace CodeAlgorithm
 {
-    internal class Solution
+    //문제 설명
+    //정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+    //제한사항
+    //0 < num1 ≤ 100
+    //0 < num2 ≤ 100
+
+    public class Quotient
     {
+        public int solution(int num1, int num2)
+        {
+            int answer = 0;
+            if (num1 <= 0 && num1 >= 100 && num2 <= 0 && num2 >= 100)
+            {
+                answer = num1 / num2;
+            }
+            return answer;
+        }
     }
 }
