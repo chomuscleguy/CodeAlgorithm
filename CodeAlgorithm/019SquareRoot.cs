663e8f47c92e1be3e21794438e6b2a59c0bfb808
조성우 <chomuscleguy@gmail.com>
2023-11-27 10:12:49 +0900

    SquareRoot

---
+++ CodeAlgorithm/019SquareRoot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CodeAlgorithm
+{
+    public class SquareRoot
+    {
+        //문제 설명
+        //임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+        //n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+        //제한 사항
+        //n은 1이상, 50000000000000 이하인 양의 정수입니다.
+        public long solution(long n)
+        {
+            long answer = 0;
+            for (long i = 0; i <= n; i++)
+            {
+                if (i * i == n)
+                {
+                    i += 1;
+                    answer = i * i;
+                    break;
+                }
+                else answer = -1;
+            }
+            return answer;
+        }
+    }
+}
