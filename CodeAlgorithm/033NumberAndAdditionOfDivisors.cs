cd32f00bfbaba2167f7b00aa1ce94a0095b326ed
조성우 <chomuscleguy@gmail.com>
2023-12-08 10:40:53 +0900

    NumberAndAdditionOfDivisors

---
+++ CodeAlgorithm/033NumberAndAdditionOfDivisors.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlgorithm
+{
+    public class NumberAndAdditionOfDivisors
+    {
+        //문제 설명
+        //두 정수 left와 right가 매개변수로 주어집니다.left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+
+        //제한사항
+        //1 ≤ left ≤ right ≤ 1,000
+        public int solution(int left, int right)
+        {
+            int answer = 0;
+            for (int i = left; i <= right; i++)
+            {
+                int divisor = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        divisor++;
+                    }
+                }
+                if (divisor % 2 == 0 && divisor != 0)
+                {
+                    answer += i;
+                }
+                else if (divisor % 2 != 0 && divisor != 0)
+                {
+                    answer -= i;
+                }
+            }
+            return answer;
+        }
+    }
+}
